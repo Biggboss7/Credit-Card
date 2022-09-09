aad828a7415731ba407a0e5a756fb7af73c3b40a
Michael <mtse431@gmail.com>
2022-09-09 17:44:29 +0700

    Give Element's IDs and Declaration of Variables
    Author changes some of input types from number to text since it is made
    easier by text input property "maxlength" to restrict inputs data

---
+++ main/script.js
@@ -0,0 +1,11 @@
+const cardNumber = document.getElementById("cardNum");
+const cardHolder = document.getElementById("cardHolder");
+const cardExpiry = document.getElementById("cardExpiry");
+const cardCVC = document.getElementById("cardCVC");
+const form = document.getElementById("confirmationForm");
+const submitBtn = document.getElementById("submitBtn");
+const userInputs = document.querySelectorAll("input");
+const inputCardNumber = document.getElementById("in-cardNumber");
+const cardDigits = [];
+
+
